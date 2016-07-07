cb05c475732c77f79eb6b9aceeab34bf3309228d
Training <Training@DIN39000560.corp.capgemini.com>
2016-07-07 10:01:37 +0530

    greeting message changes

--- ProductDemo/src/Mainclass.java
+++ ProductDemo/src/Mainclass.java
@@ -4,7 +4,13 @@ public class Mainclass {
 	public static void main(String[] args) {
 		ProductApp app = new ProductApp();
 		app.display();
+		greetings();
 
+	}
+	
+	public static void greetings()
+	{
+		System.out.println("greetings message!!!");
 	}
 
 }
